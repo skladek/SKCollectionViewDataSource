18cdbff02ab82f89fd80660472fc9e4502a97222
Sean Kladek <sean@303software.com>
2017-05-15 14:23:50 -0600

    Initial project commit

---
+++ CollectionViewDataSource/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CollectionViewDataSource
+//
+//  Created by Sean on 5/15/17.
+//  Copyright © 2017 Sean Kladek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
