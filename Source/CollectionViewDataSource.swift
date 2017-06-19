b716d7cd8dcad038d72f9c20f7d21ae8b273e0f0
Sean Kladek <sean@303software.com>
2017-06-19 11:26:59 -0600

    Moved the delegate to its own file

--- Source/CollectionViewDataSource.swift
+++ Source/CollectionViewDataSource.swift
@@ -8,27 +8,6 @@
 
 import UIKit
 
-@objc
-public protocol CollectionViewDataSourceDelegate {
-    @objc
-    optional func collectionView(_ collectionView: UICollectionView, canMoveItemAt indexPath: IndexPath) -> Bool
-
-    @objc
-    optional func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell?
-
-    @objc
-    optional func collectionView(_ collectionView: UICollectionView, moveItemAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath)
-
-    @objc
-    optional func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int
-
-    @objc
-    optional func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView?
-
-    @objc
-    optional func numberOfSections(in collectionView: UICollectionView) -> Int
-}
-
 /// Provides a configuration object for collection view cells.
 public struct CellConfiguration<T> {
     let presenter: CollectionViewDataSource<T>.CellPresenter?
